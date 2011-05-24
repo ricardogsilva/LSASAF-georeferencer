fdd3d56ba6ee5c5df30ae4dc1dbba099c6a13246
Ricardo Silva <ricardo.garcia.silva@gmail.com>
2011-05-24 18:28:13 +0100

    Still working on the GUI.

--- HDF5Georeferencer.py
+++ HDF5Georeferencer.py
@@ -31,25 +31,52 @@ class HDF5Georeferencer(QDialog, Ui_Form):
         super(HDF5Georeferencer, self).__init__(parent)
         self.setupUi(self)
         self.connect(self.inputFilesPB, SIGNAL("clicked()"), self.get_files)
-        self.toggle_widgets([self.datasetsLV, self.wgs84RB, self.customProjRB,
+        self.connect(self.loadFilePB, SIGNAL("clicked()"), self.get_datasets)
+        self.toggle_widgets([self.datasetsLW, self.wgs84RB, self.customProjRB,
                             self.outputDirLE, self.outputDirPB,
                             self.deleteIntermediaryCB, self.processFilesPB])
 
-    def toggle_widgets(self, widgetList):
+    def toggle_widgets(self, widgetList, forceEnabled=False):
         """Toggles widgets' enabled state."""
 
-        for wid in widgetList:
-            wid.setEnabled(not wid.isEnabled())
+        if forceEnabled:
+            for wid in widgetList:
+                wid.setEnabled(True)
+        else:
+            for wid in widgetList:
+                wid.setEnabled(not wid.isEnabled())
 
     def get_files(self):
+        self.logger.debug("get_files method called.")
         filePaths = QFileDialog.getOpenFileNames(self, "Select HDF5 files")
         if len(filePaths) > 0:
             self.logger.debug("Some files have been selected.")
             self.inputFilesLE.setText(";".join([str(p) for p in filePaths]))
         else:
             self.logger.debug("No files have been selected.")
+        self.logger.debug("get_files method exiting.")
 
+    def get_datasets(self):
+        """Open one of the user selected files and extract dataset names."""
 
+        self.logger.debug("get_datasets method called.")
+        filePaths = [str(s) for s in self.inputFilesLE.text().split(";")]
+        self.datasetsLW.clear()
+        try:
+            h5f = H5Georef(filePaths[0])
+            datasets = h5f.arrays.keys()
+            mainDataset = [name for name, params in h5f.arrays.iteritems() \
+                          if params.get("mainArray")][0]
+            self.logger.info("datasets: %s" % datasets)
+            self.logger.info("main dataset: %s" % mainDataset)
+            for datasetName in datasets:
+                self.datasetsLW.addItem(datasetName)
+            if len(datasets) > 0:
+                self.toggle_widgets([self.datasetsLW, self.wgs84RB,
+                                    self.customProjRB], True)
+        except IOError, msg:
+            self.logger.error(msg)
+        self.logger.debug("get_datasets method exiting.")
 
 def create_logger(logLevel="info"):
     level = eval("logging.%s" % (logLevel.upper()))
